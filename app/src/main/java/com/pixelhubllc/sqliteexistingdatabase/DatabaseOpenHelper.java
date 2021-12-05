767f0a6c9eac35a27c28196f6fdb1c6cdf22edf5
Mahmudul Hasan Tarek <33068775+tarek0695@users.noreply.github.com>
2021-12-05 23:52:08 +0600

    c

--- app/src/main/java/com/pixelhubllc/sqliteexistingdatabase/DatabaseOpenHelper.java
+++ app/src/main/java/com/pixelhubllc/sqliteexistingdatabase/DatabaseOpenHelper.java
@@ -2,15 +2,14 @@ package com.pixelhubllc.sqliteexistingdatabase;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "eng_dictionary.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        setForcedUpgrade();
     }
-
 }
--- app/src/main/java/com/pixelhubllc/sqliteexistingdatabase/MainActivity.java
+++ app/src/main/java/com/pixelhubllc/sqliteexistingdatabase/MainActivity.java
@@ -1,9 +1,19 @@
 package com.pixelhubllc.sqliteexistingdatabase;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +21,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private SearchView searchView;
+    private AlertDialog alertDialog;
+    private Context context;
+    private DatabaseAccess databaseAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listview);
         searchView = findViewById(R.id.search_bar);
 
-        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess = DatabaseAccess.getInstance(this);
         databaseAccess.open();
+
         final List<String> quotes = databaseAccess.getQuotes();
         databaseAccess.close();
 
@@ -47,6 +61,14 @@ public class MainActivity extends AppCompatActivity {
                 listView.setAdapter(adapter);
 
                 return true;
+            }
+        });
+        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("position", "onItemClick: " +position );
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
